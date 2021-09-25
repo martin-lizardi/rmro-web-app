bcf40f8d1bb0ebb49b60674f30f788ff654ec818
Martin Lizardi <martin.lizardi109@gmail.com>
2021-09-25 09:48:26 -0500

    Install Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_BzgUmCVHZkoJ32F74YKFcdR6myOdVIU',
+    authDomain: 'rmro-5d8ff.firebaseapp.com',
+    projectId: 'rmro-5d8ff',
+    storageBucket: 'rmro-5d8ff.appspot.com',
+    messagingSenderId: '224913199250',
+    appId: '1:224913199250:web:0a5024a4f0079962f1564b',
+  },
 };
